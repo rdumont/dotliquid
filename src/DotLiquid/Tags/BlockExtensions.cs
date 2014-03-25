e681b3db6fa0a39885ec196111b1b87feb534570
Rodrigo Dumont <rlsdumont@gmail.com>
2014-03-25 16:56:24 -0300

    Allows block tag to know if it has been overridden

---
+++ src/DotLiquid/Tags/BlockExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace DotLiquid.Tags
+{
+    public static class BlockExtensions
+    {
+        public static bool IsOverridden(this Block @this, Context context)
+        {
+            var blockState = BlockRenderState.Find(context);
+            return blockState.NodeLists.ContainsKey(@this);
+        }
+    }
+}
